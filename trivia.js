a4b4e61c1e1cf41ba7e61b39efa93a6fd83b4ba4
jdabis9858 <jdabis4281994@gmail.com>
2018-10-20 09:12:34 -0700

    edits1

---
+++ trivia.js
@@ -0,0 +1,24 @@
+var time = ""
+var questions = ""
+var answers = ""
+
+$(document).ready(function() {
+    
+
+    $("#start").on("click", function() {
+        $("#start").hide();
+        $("#question").html("What color is the sky?");
+        $("#answer").html("red, blue, or green");
+    });
+
+
+
+
+
+
+
+
+
+
+
+});
